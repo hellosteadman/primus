9476b8912fbd0a8f69a972b78a3bc8d31a01526b
Mark Steadman <marksteadman@me.com>
2013-09-30 22:52:28 +0100

    CSS shuffles
    Moved CSS declarations around to ensure style.css takes precident

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
 		<title><?php wp_title(''); ?></title>
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
-		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<?php if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); ?>
 		<?php wp_head(); ?>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
 	</head>
 	
 	<body <?php body_class('has-' . get_theme_mod('navbar_placement')); ?> data-spy="scroll" data-target="#sidebar-1">
